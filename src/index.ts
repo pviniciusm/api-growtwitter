e3d5f16614b32b47c12cd41acfa7db316d9d53b1
Paulo <pvinics@gmail.com>
2023-10-09 19:44:10 -0300

    chore: api basic config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello world");
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(3333, () => {
+    console.log("API is running");
+});
