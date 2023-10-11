00f3b6444909285fd266106df8d49f086ac6d95c
Paulo <pvinics@gmail.com>
2023-10-11 20:38:36 -0300

    build: prepare for deploy

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import { userRoutes } from "./routes/user.routes";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(express.json());
@@ -8,6 +10,6 @@ app.use(cors());
 
 app.use("/user", userRoutes());
 
-app.listen(3333, () => {
+app.listen(process.env.PORT, () => {
     console.log("API is running");
 });
