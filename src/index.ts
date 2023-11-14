e2dd555b79bb4a959612cd4b16ecde7196794a3d
Paulo <pvinics@gmail.com>
2023-11-14 20:16:25 -0300

    docs: initial version of swagger documentation

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,17 @@ import { userRoutes } from "./routes/user.routes";
 import * as dotenv from "dotenv";
 dotenv.config();
 
+import swaggerUi from "swagger-ui-express";
+import swaggerDoc from "./docs/swagger.json";
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use("/user", userRoutes());
+
+app.use("/docs", swaggerUi.serve);
+app.get("/docs", swaggerUi.setup(swaggerDoc));
 
 app.listen(process.env.PORT, () => {
     console.log("API is running");
